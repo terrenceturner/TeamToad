9e4e00fce6507666cec4319f37b0ed5cfaa727f8
andresnew00 <andresnew00@hotmail.com>
2019-10-02 14:33:27 -0400

    created a dao and dao implementation for book

---
+++ book-service/src/test/java/com/toad/bookservice/dao/BookDaoJdbcTemplateImplTest.java
@@ -0,0 +1,37 @@
+package com.toad.bookservice.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookDaoJdbcTemplateImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createBook() {
+    }
+
+    @Test
+    public void getBookbyId() {
+    }
+
+    @Test
+    public void getAllBooks() {
+    }
+
+    @Test
+    public void updateBook() {
+    }
+
+    @Test
+    public void deleteBook() {
+    }
+
+    @Test
+    public void mapRowToBook() {
+    }
+}

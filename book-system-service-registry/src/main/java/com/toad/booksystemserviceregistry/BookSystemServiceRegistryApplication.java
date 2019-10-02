19990b7db6d98267e84e98ec907e9966a0eaf0a6
Ovie <you@example.com>
2019-10-02 13:09:36 -0400

    basic fole structure +pom filesand bootstrap.properties

---
+++ book-system-service-registry/src/main/java/com/toad/booksystemserviceregistry/BookSystemServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.toad.booksystemserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSystemServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSystemServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ note-queue-consumer/src/main/java/com/toad/notequeueconsumer/NoteQueueConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.toad.notequeueconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteQueueConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteQueueConsumerApplication.class, args);
+	}
+
+}

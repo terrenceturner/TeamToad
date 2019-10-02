cb1241dc7ca5d0541786e11de96a37e681a75b98
andresnew00 <andresnew00@hotmail.com>
2019-10-02 15:08:07 -0400

    dao and daoimp for book

--- book-service/src/main/java/com/toad/bookservice/dao/BookDaoJdbcTemplateImpl.java
+++ book-service/src/main/java/com/toad/bookservice/dao/BookDaoJdbcTemplateImpl.java
@@ -1,7 +1,71 @@
 package com.toad.bookservice.dao;
 
+import com.toad.bookservice.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 public class BookDaoJdbcTemplateImpl implements BookDao {
+
+    //prepared statements
+    public static final String INSERT_BOOK =
+            "insert into book (title, author) values (?,?)";
+    public static final String SELECT_BOOK_BY_ID =
+            "select * from book where book_id = ?";
+    public static final String SELECT_ALL_BOOKS =
+            "select * from book";
+    public static final String UPDATE_BOOK =
+            "update book set title = ?, author = ? where book_id = ?";
+    public static final String DELETE_BOOK =
+            "delete from book where book_id = ?";
+
+    @Autowired
+    JdbcTemplate temp;
+
+    public BookDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.temp = jdbcTemplate;
+    }
+
+    @Override
+    public Book createBook(Book book) {
+        temp.update(INSERT_BOOK,
+                book.getTitle(),
+                book.getAuthor());
+
+        int id = temp.queryForObject("select last_insert_id()",Integer.class);
+        book.setBookId(id);
+        return book;
+    }
+    @Override
+    public Book getBookbyId(int id) {
+        return temp.queryForObject(SELECT_BOOK_BY_ID,this::MapRowToBook,id);
+    }
+    @Override
+    public List<Book> getAllBooks() {
+        return temp.query(SELECT_ALL_BOOKS,this::MapRowToBook);
+    }
+    @Override
+    public void updateBook(int bookId) {
+        Book book = getBookbyId(bookId);
+        temp.update(UPDATE_BOOK,
+                book.getTitle(),
+                book.getAuthor(),
+                book.getBookId());
+    }
+    @Override
+    public void deleteBook(int id) {
+        temp.update(DELETE_BOOK,id);
+    }
+    public Book MapRowToBook(ResultSet rs, int num) throws SQLException {
+        Book book = new Book();
+        book.setBookId(rs.getInt("book_id"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthor(rs.getString("author"));
+        return book;
+    }
 }
--- book-service/src/test/java/com/toad/bookservice/dao/BookDaoJdbcTemplateImplTest.java
+++ book-service/src/test/java/com/toad/bookservice/dao/BookDaoJdbcTemplateImplTest.java
@@ -1,22 +1,49 @@
 package com.toad.bookservice.dao;
 
+import com.toad.bookservice.model.Book;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class BookDaoJdbcTemplateImplTest {
+
+    @Autowired
+    BookDao bookDao;
+
+    private Book book;
 
     @Before
     public void setUp() throws Exception {
+
+        bookDao.getAllBooks().forEach(book -> {
+            bookDao.deleteBook(book.getBookId());
+        });
+
+        book = new Book();
+        book.setTitle("Title");
+        book.setAuthor("Author");
+
     }
 
     @Test
     public void createBook() {
+        Book book1 = bookDao.createBook(book);
+
+        assertEquals(1,book1.getBookId());
+        assertEquals(book.getAuthor(),book1.getAuthor());
+        assertEquals(book.getTitle(),book1.getTitle());
     }
 
     @Test
     public void getBookbyId() {
+
     }
 
     @Test

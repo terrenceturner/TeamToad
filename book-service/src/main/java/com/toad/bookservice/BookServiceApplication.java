1666d6a5ef2f6b37925a00ed2a8979afce873aec
andresnew00 <andresnew00@hotmail.com>
2019-10-03 11:14:12 -0400

    rabbit connection

--- book-service/src/main/java/com/toad/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/com/toad/bookservice/BookServiceApplication.java
@@ -1,13 +1,30 @@
 package com.toad.bookservice;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookServiceApplication {
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+		return rabbitTemplate;
+
+	}
+	@Bean
+	public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+		return new Jackson2JsonMessageConverter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
 
 }
+
